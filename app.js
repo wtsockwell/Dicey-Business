715f2009d99f4ced83422bec09dbef8f084256f4
William Taylor Sockwell <wtsockwell@yahoo.com>
2021-02-15 04:22:37 -0600

    added functionality for updating roll values through the button, as well as shuffled some lines around to allow updating of values

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 let value
+let dice = []
 class Die{
     constructor(){
-        this.roll()
-        this.value = $("<h1></h1>").text(value);
+        this.value = $("<h1 id='value'></h1>");
         this.div = $("<div class='die'></div>");
+        this.roll()
         $("#container").append(this.div);
         this.value.appendTo(this.div)
     }
     roll(){
         value = (Math.floor(Math.random()*6)+1)
+        this.value.text(value)
     }
 }
 
 $("#makeDie").click(function (){
     let die = new Die
-    die.roll();
+    dice.push(die)
     $("#container").append(die);
+})
+
+$("#rollDie").click(function (){
+    dice.forEach(die =>{
+        die.roll()
+    })
 })
