8af61158eaaf8afc8e0e9f02592e679628471a05
William Taylor Sockwell <wtsockwell@yahoo.com>
2021-02-15 06:03:28 -0600

    added ability to count the values of each die and add them together

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 let value
 let dice = []
-class Die{
-    constructor(){
-        this.value = $("<h1 id='value'></h1>");
+let diceValue = []
+class Die {
+    constructor() {
+        this.value = $("<h1></h1>");
         this.div = $("<div class='die'></div>");
         this.roll()
         $("#container").append(this.div);
         this.value.appendTo(this.div)
     }
-    roll(){
-        value = (Math.floor(Math.random()*6)+1)
+    roll() {
+        value = (Math.floor(Math.random() * 6) + 1)
         this.value.text(value)
     }
 }
 
-$("#makeDie").click(function (){
+$("#makeDie").click(function () {
     let die = new Die
     dice.push(die)
+    diceValue.push(Number(die.value[0].textContent))
     $("#container").append(die);
 })
 
-$("#rollDie").click(function (){
-    dice.forEach(die =>{
+$("#rollDie").click(function () {
+    dice.forEach(die => {
         die.roll()
     })
+})
+
+$("#sumDice").click(() => {
+    function sum(a, b) {
+        return a + b
+    }
+    let total = diceValue.reduce(sum)
+    alert(`The total is ${total}`)
 })
